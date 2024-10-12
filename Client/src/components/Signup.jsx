23f4626d99ced5820cc92beaed176e004bcbea9d
Abdul Raoof <raoofkottayil@gmail.com>
2024-10-12 11:35:28 +0530

    feat(page): Add recoil to toggle login and signup

--- Client/src/components/Signup.jsx
+++ Client/src/components/Signup.jsx
@@ -16,9 +16,12 @@ import {
 } from '@chakra-ui/react'
 import { useState } from 'react'
 import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons'
+import { useSetRecoilState } from 'recoil'
+import authScreenAtom from '../atoms/authAtom'
 
 export default function Signup() {
   const [showPassword, setShowPassword] = useState(false)
+  const setAuthScreen = useSetRecoilState(authScreenAtom)
 
   return (
     <Flex
@@ -82,7 +85,8 @@ export default function Signup() {
             </Stack>
             <Stack pt={6}>
               <Text align={'center'}>
-                Already a user? <Link color={'blue.400'}>Login</Link>
+                Already a user? <Link color={'blue.400'}
+                onClick={()=> setAuthScreen("login")}>Login</Link>
               </Text>
             </Stack>
           </Stack>
--- Client/src/pages/AuthPage.jsx
+++ Client/src/pages/AuthPage.jsx
@@ -1,12 +1,12 @@
-import Login from "../components/Login"
-import Signup from "../components/Signup"
+import { useRecoilValue } from "recoil";
+import Login from "../components/Login";
+import Signup from "../components/Signup";
+import authScreenAtom from "../atoms/authAtom";
 
 const AuthPage = () => {
-  return (
-    <>
-      <Login/>
-    </>
-  )
-}
+  const authScreenState = useRecoilValue(authScreenAtom);
+  console.log(authScreenState);
+  return <>{authScreenState === "login" ? <Login /> : <Signup />}</>;
+};
 
-export default AuthPage
+export default AuthPage;
