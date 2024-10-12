52c9e45d036d3eeb7484c02587b88b40dc749e3c
Abdul Raoof <raoofkottayil@gmail.com>
2024-10-12 23:13:28 +0530

    fix(client): Fix vite and signup file error

--- Client/src/components/Signup.jsx
+++ Client/src/components/Signup.jsx
@@ -13,58 +13,116 @@ import {
   Text,
   useColorModeValue,
   Link,
-} from '@chakra-ui/react'
-import { useState } from 'react'
-import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons'
-import { useSetRecoilState } from 'recoil'
-import authScreenAtom from '../atoms/authAtom'
+  useToast,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
+import { useSetRecoilState } from "recoil";
+import authScreenAtom from "../atoms/authAtom";
 
 export default function Signup() {
-  const [showPassword, setShowPassword] = useState(false)
-  const setAuthScreen = useSetRecoilState(authScreenAtom)
+  const [showPassword, setShowPassword] = useState(false);
+  const setAuthScreen = useSetRecoilState(authScreenAtom);
+  const [inputs, setInputs] = useState({
+    name: "",
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const toast = useToast();
+
+  const handleSignup = async () => {
+    try {
+      const res = await fetch("/api/users/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(inputs),
+      });
+      const data = await res.json();
+      if (data.error) {
+        toast({
+          title: "Error",
+          describtion: data.error,
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+        });
+        return;
+      }
+      localStorage.setItem("user-threads", JSON.stringify(data))
+
+    } catch (error) {
+      console.log("Error in handleSignup:", error);
+    }
+  };
 
   return (
-    <Flex
-      align={'center'}
-      justify={'center'}>
-      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-        <Stack align={'center'}>
-          <Heading fontSize={'4xl'} textAlign={'center'}>
+    <Flex align={"center"} justify={"center"}>
+      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+        <Stack align={"center"}>
+          <Heading fontSize={"4xl"} textAlign={"center"}>
             Sign up
           </Heading>
         </Stack>
         <Box
-          rounded={'lg'}
-          bg={useColorModeValue('white', 'gray.dark')}
-          boxShadow={'lg'}
-          p={8}>
+          rounded={"lg"}
+          bg={useColorModeValue("white", "gray.dark")}
+          boxShadow={"lg"}
+          p={8}
+        >
           <Stack spacing={4}>
             <HStack>
               <Box>
-                <FormControl  isRequired>
+                <FormControl isRequired>
                   <FormLabel>Full name</FormLabel>
-                  <Input type="text" />
+                  <Input
+                    type="text"
+                    onChange={(e) =>
+                      setInputs({ ...inputs, name: e.target.value })
+                    }
+                  />
                 </FormControl>
               </Box>
               <Box>
                 <FormControl isRequired>
                   <FormLabel>Username</FormLabel>
-                  <Input type="text" />
+                  <Input
+                    type="text"
+                    onChange={(e) =>
+                      setInputs({ ...inputs, username: e.target.value })
+                    }
+                  />
                 </FormControl>
               </Box>
             </HStack>
             <FormControl isRequired>
               <FormLabel>Email address</FormLabel>
-              <Input type="email" />
+              <Input
+                type="email"
+                onChange={(e) =>
+                  setInputs({ ...inputs, email: e.target.value })
+                }
+              />
             </FormControl>
-            <FormControl  isRequired>
+            <FormControl isRequired>
               <FormLabel>Password</FormLabel>
               <InputGroup>
-                <Input type={showPassword ? 'text' : 'password'} />
-                <InputRightElement h={'full'}>
+                <Input
+                  type={showPassword ? "text" : "password"}
+                  onChange={(e) =>
+                    setInputs({ ...inputs, password: e.target.value })
+                  }
+                />
+                <InputRightElement h={"full"}>
                   <Button
-                    variant={'ghost'}
-                    onClick={() => setShowPassword((showPassword) => !showPassword)}>
+                    variant={"ghost"}
+                    onClick={() =>
+                      setShowPassword((showPassword) => !showPassword)
+                    }
+                  >
                     {showPassword ? <ViewIcon /> : <ViewOffIcon />}
                   </Button>
                 </InputRightElement>
@@ -74,24 +132,27 @@ export default function Signup() {
               <Button
                 loadingText="Submitting"
                 size="lg"
-                bg={useColorModeValue("gray.600","gray.700")}
-                color={'white'}
+                bg={useColorModeValue("gray.600", "gray.700")}
+                color={"white"}
                 _hover={{
-                  bg: useColorModeValue("gray.700","gray.800"),
+                  bg: useColorModeValue("gray.700", "gray.800"),
                 }}
-                >
+                onClick={handleSignup}
+              >
                 Sign up
               </Button>
             </Stack>
             <Stack pt={6}>
-              <Text align={'center'}>
-                Already a user? <Link color={'blue.400'}
-                onClick={()=> setAuthScreen("login")}>Login</Link>
+              <Text align={"center"}>
+                Already a user?{" "}
+                <Link color={"blue.400"} onClick={() => setAuthScreen("login")}>
+                  Login
+                </Link>
               </Text>
             </Stack>
           </Stack>
         </Box>
       </Stack>
     </Flex>
-  )
+  );
 }
