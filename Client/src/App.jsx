b42b4e465a91eef99244de1e3beb7d7b95a22866
Abdul Raoof <raoofkottayil@gmail.com>
2024-10-14 15:17:23 +0530

    feat(client): Connect update route with update page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -8,6 +8,7 @@ import AuthPage from "./pages/AuthPage";
 import { useRecoilValue } from "recoil";
 import userAtom from "./atoms/userAtom";
 import LogoutButton from "./components/logoutButton";
+import UpdateProfilePage from "./pages/UpdateProfilePage";
 
 function App() {
   const user = useRecoilValue(userAtom);
@@ -24,6 +25,11 @@ function App() {
           path="/auth"
           element={!user ? <AuthPage /> : <Navigate to="/" />}
         />
+        <Route
+          path="/update"
+          element={user ? <UpdateProfilePage /> : <Navigate to="/auth" />}
+        />
+        
         <Route path="/:username" element={<UserPage />} />
         <Route path="/:username/post/:pid" element={<PostPage />} />
       </Routes>
