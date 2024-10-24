5b2f98d5d03325fa6b49bc26238203244135aac0
Abdul Raoof <raoofkottayil@gmail.com>
2024-10-24 10:09:51 +0530

    feat(Server): Create route to post message

---
+++ Server/controllers/messageController.js
@@ -0,0 +1,45 @@
+import Conversation from "../models/conversationModel.js";
+import Message from "../models/messageModel.js";
+
+async function sendMessage(req, res) {
+  try {
+    const { recipientId, message } = req.body;
+    const senderId = req.user._id;
+
+    let conversation = await Conversation.findOne({
+      participants: { $all: [senderId, recipientId] },
+    });
+    if (!conversation) {
+      conversation = new Conversation({
+        participants: [senderId, recipientId],
+        lastMessage: {
+          text: message,
+          sender: senderId,
+        },
+      });
+      await conversation.save();
+    }
+
+    const newMessage = new Message({
+      conversationId: conversation._id,
+      sender: senderId,
+      text: message,
+    });
+
+    await Promise.all([
+      newMessage.save(),
+      conversation.updateOne({
+        lastMessage: {
+          text: message,
+          sender: senderId,
+        },
+      }),
+    ]);
+
+    res.status(201).json(newMessage);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+export { sendMessage };
--- Server/server.js
+++ Server/server.js
@@ -4,6 +4,7 @@ import connectDB from "./db/connectDB.js"
 import cookieParser from "cookie-parser"
 import userRoutes from "./routes/userRoutes.js"
 import postRoutes from "./routes/postRoutes.js"
+import messageRoutes from "./routes/messageRoutes.js"
 import {v2 as cloudinary} from "cloudinary"
 
 dotenv.config()
@@ -24,5 +25,6 @@ app.use(cookieParser())
 
 app.use("/api/users",userRoutes)
 app.use("/api/posts",postRoutes)
+app.use("/api/message",messageRoutes)
 
 app.listen (port, () => console.log(`server is running on port ${port}`))
