9dc67116984d0c923950b5ce27b227a1bcddbc51
Abdul Raoof <raoofkottayil@gmail.com>
2024-10-26 11:08:58 +0530

    feat(Server): Implement socket.io

--- Server/server.js
+++ Server/server.js
@@ -6,16 +6,19 @@ import userRoutes from "./routes/userRoutes.js"
 import postRoutes from "./routes/postRoutes.js"
 import messageRoutes from "./routes/messageRoutes.js"
 import {v2 as cloudinary} from "cloudinary"
+import {app ,server} from "./socket/socket.js"
+
 
 dotenv.config()
 connectDB()
+
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
 })
 
-const app = express();
+
 const port = process.env.PORT || 5000;
 
 app.use(express.json({ limit: "50mb" }))
@@ -27,4 +30,4 @@ app.use("/api/users",userRoutes)
 app.use("/api/posts",postRoutes)
 app.use("/api/messages",messageRoutes)
 
-app.listen (port, () => console.log(`server is running on port ${port}`))
+server.listen (port, () => console.log(`server is running on port ${port}`))
